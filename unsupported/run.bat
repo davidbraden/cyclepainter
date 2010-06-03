271d040afc36f98f849362715f64320705e4dcf7
Tim Northover <T.P.Northover@sms.ed.ac.uk>
2010-06-03 10:21:38 +0100

    Add a windows launcher.
    Very experimental, but known to work on at least one computer.

---
+++ unsupported/run.bat
@@ -0,0 +1,5 @@
+set MAPLE=C:\Program Files\Maple 13
+
+
+java -Djava.library.path="%MAPLE%\bin.win" -classpath "%MAPLE%\java\externalcall.jar;%MAPLE%\java\jopenmaple.jar;dist/CyclePainter.jar" cyclepainter.Main
+
